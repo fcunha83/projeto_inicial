9c83cf0bd36293bcc6b463c1e4c435523786ee2d
Fábio Pereira da Cunha <fcunha@osg.com.br>
2024-08-22 11:21:17 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 41
+nome = "Fábio"
+
+# Print
+print(idade)
+ 
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+
